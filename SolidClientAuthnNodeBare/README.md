fc01c813bf15d253f6e2f3d425e6913da6f48d0d
uo269787 <uo269787@uniovi.es>
2021-04-11 20:25:44 +0200

    Update readme

--- SolidClientAuthnNodeBare/README.md
+++ SolidClientAuthnNodeBare/README.md
@@ -1,1 +1,3 @@
 Needs crypto and http2
+
+Stuck when trying to load zlib: _stream_transform doesn't get picked up for some reason even if it is installed.
--- SolidClientAuthnNodeBare/shim.js
+++ SolidClientAuthnNodeBare/shim.js
@@ -23,5 +23,4 @@ if (typeof localStorage !== 'undefined') {
 
 // If using the crypto shim, uncomment the following line to ensure
 // crypto is loaded first, so it can populate global.crypto
-import 'react-native-get-random-values';
-require('crypto')
+// require('crypto')
